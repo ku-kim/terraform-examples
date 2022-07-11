d4287a12e7aa09223c634809d9213f2cf0cecdbe
“kukim” <kukim.dev@gmail.com>
2022-07-11 17:44:12 +0900

    feat: :sparkles: public/private 이름의 서브넷 2개 생성

--- vpc/create-vpc/vpc.tf
+++ vpc/create-vpc/vpc.tf
@@ -5,3 +5,25 @@ resource "aws_vpc" "main" {
         Name = "terraform-kukim-vpc"
     }
 }
+
+resource "aws_subnet" "public_subnet" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.10.0/24"
+
+    availability_zone = "ap-northeast-2a"
+
+    tags = {
+        Name = "terraform-kukim-public-subnet"
+    }
+
+}
+
+resource "aws_subnet" "private_subnet" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.11.0/24"
+
+    tags = {
+        Name = "terraform-kukim-private-subnet"
+    }
+
+}
