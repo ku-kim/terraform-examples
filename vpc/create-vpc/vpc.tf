a811ca483f4d6c54f56ac50e22c26791c92d2fb8
“kukim” <kukim.dev@gmail.com>
2022-07-11 17:47:53 +0900

    feat: :sparkles: 라우팅 테이블 - 서브넷 연결

--- vpc/create-vpc/vpc.tf
+++ vpc/create-vpc/vpc.tf
@@ -68,3 +68,13 @@ resource "aws_route_table" "private" {
         Name = "terraform-kukim-rt-private"    
     }
 }
+
+resource "aws_route_table_association" "route_table_association_public" {
+    subnet_id = aws_subnet.public_subnet.id
+    route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "route_table_association_private" {
+    subnet_id = aws_subnet.private_subnet.id
+    route_table_id = aws_route_table.private.id
+}
