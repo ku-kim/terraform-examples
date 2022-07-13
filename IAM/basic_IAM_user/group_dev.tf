450b2fdf547449f1ac0a10cbc490b3b2c39db6de
“kukim” <kukim.dev@gmail.com>
2022-07-13 22:50:55 +0900

    feat: :sparkles: IAM Group에 IAM user 추가

--- IAM/basic_IAM_user/group_dev.tf
+++ IAM/basic_IAM_user/group_dev.tf
@@ -1,3 +1,14 @@
 resource "aws_iam_group" "dev_group" {
     name = "dev"
 }
+
+resource "aws_iam_group_membership" "dev_group_membership" {
+
+    name = aws_iam_group.dev_group.name
+
+    users = [
+        aws_iam_user.terraform_kukim_test.name
+    ]
+
+    group = aws_iam_group.dev_group.name
+}
