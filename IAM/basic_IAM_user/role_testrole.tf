18e514ca1e85592f165b6b09f7416c1a35326f46
“kukim” <kukim.dev@gmail.com>
2022-07-13 23:01:26 +0900

    feat: :sparkles: IAM Role 생성

---
+++ IAM/basic_IAM_user/role_testrole.tf
@@ -0,0 +1,20 @@
+resource "aws_iam_role" "hello" {
+  name               = "hello-iam-role"
+  path               = "/"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+
+}
