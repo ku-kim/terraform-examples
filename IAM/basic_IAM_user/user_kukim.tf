6864d8f287547376ebe241151e36cbed0a0992cc
“kukim” <kukim.dev@gmail.com>
2022-07-13 23:10:11 +0900

    feat: :sparkles: IAM user에 custom admin policy 추가

--- IAM/basic_IAM_user/user_kukim.tf
+++ IAM/basic_IAM_user/user_kukim.tf
@@ -1,3 +1,25 @@
 resource "aws_iam_user" "terraform_kukim_test" {
     name = "terraform_kukim_test"
 }
+
+resource "aws_iam_user_policy" "art_devops_black" {
+  name  = "super-admin"
+  user  = aws_iam_user.terraform_kukim_test.name
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "*"
+            ],
+            "Resource": [
+                "*"
+            ]
+        }
+    ]
+}
+EOF
+}
